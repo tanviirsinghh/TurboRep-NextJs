670e7c8535b728cc43c91c82eed43c0f8f53bb9e
iTanvirSingh <71079808+tanviirsinghh@users.noreply.github.com>
2025-03-04 23:49:32 +0530

    firsrt commit

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div  className="text-xl text-center text-violet-400">
-      welcome to user app
+      welcome to user app friend
     </div>
   );
 }
